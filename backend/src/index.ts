eaff4c4bd12fd04c2dd28e9e3de6f25a24af9b67
hungnguyen3 <hachi.codingcat@gmail.com>
2023-01-22 04:29:39 -0800

    firebase thing fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,10 +24,16 @@ const admin = require('firebase-admin');
 // 	}),
 // });
 
+// const firebase_private_key_b64 = Buffer.from(
+// 	process.env.FIREBASE_PRIVATE_KEY!,
+// 	'base64'
+// );
+// const firebase_private_key = firebase_private_key_b64.toString('utf8');
+
 admin.initializeApp({
 	credential: admin.credential.cert({
 		project_id: process.env.FIREBASE_PROJECT_ID,
-		private_key: process.env.FIREBASE_PRIVATE_KEY,
+		private_key: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
 		client_email: process.env.FIREBASE_CLIENT_EMAIL,
 	}),
 });
